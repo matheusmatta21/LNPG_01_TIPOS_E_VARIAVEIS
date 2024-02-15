37da7958dcb1c9859d2a1f1657741b4f6ec71dad
Matheus Matta <138629402+matheusmatta21@users.noreply.github.com>
2024-02-15 14:58:42 -0300

    Calculadora Simples em Haskell

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-module Calculadora where
+module Main where
 
 mediaPonderada :: Double -> Double -> Double -> Double -> Double -> Double -> Double
 mediaPonderada x1 x2 x3 y1 y2 y3 = ((x1 * y1) + (x2 * y2) + (x3 * y3)) / ((y1 + y2 + y3))
